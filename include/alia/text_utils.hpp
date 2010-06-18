20db26d18edbdbca66d40950408347307ab159ef
svn <svn@d349cc88-6891-11dd-8000-800000000000>
2010-06-18 16:21:51 +0000

    Legacy SVN commit (r328)

--- include/alia/text_utils.hpp
+++ include/alia/text_utils.hpp
@@ -3,6 +3,8 @@
 
 #include <boost/lexical_cast.hpp>
 #include <boost/format.hpp>
+#include <boost/numeric/conversion/cast.hpp>
+#include <alia/typedefs.hpp>
 
 namespace alia {
 
@@ -53,6 +55,45 @@ bool from_string(T* value, std::string const& str, std::string* message)
         return false;
     }
 }
+
+template<class T>
+bool integer_from_string(T* value, std::string const& str,
+    std::string* message)
+{
+    try
+    {
+        int64 n = boost::lexical_cast<int64>(str);
+        *value = boost::numeric_cast<T>(n);
+        return true;
+    }
+    catch (boost::bad_lexical_cast)
+    {
+        *message = std::string("This input expects ") +
+            type_to_string<T>::get() + ".";
+        return false;
+    }
+    catch (boost::bad_numeric_cast&)
+    {
+        *message = "integer out of range";
+        return false;
+    }
+}
+
+#define ALIA_INTEGER_FROM_STRING(T) \
+    static inline bool from_string( \
+        T* value, std::string const& str, std::string* message) \
+    { return integer_from_string(value, str, message); }
+
+ALIA_INTEGER_FROM_STRING(int)
+ALIA_INTEGER_FROM_STRING(unsigned)
+ALIA_INTEGER_FROM_STRING(int64)
+ALIA_INTEGER_FROM_STRING(uint64)
+ALIA_INTEGER_FROM_STRING(int32)
+ALIA_INTEGER_FROM_STRING(uint32)
+ALIA_INTEGER_FROM_STRING(int16)
+ALIA_INTEGER_FROM_STRING(uint16)
+ALIA_INTEGER_FROM_STRING(int8)
+ALIA_INTEGER_FROM_STRING(uint8)
 
 }
 
