4af4c91ca87cb9777c3db8b307fa2121e9026014
Thomas Madden <tmadden@mgh.harvard.edu>
2018-02-28 16:02:45 -0500

    Fix npm installation.

--- scripts/set-up-system.sh
+++ scripts/set-up-system.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Set up a fresh Ubuntu installation so that it can build alia.
+# This should be run with sudo.
 # (This is currently tuned to support Ubuntu Trusty.)
 echo "Setting up system..."
 set -x -e
@@ -11,6 +12,6 @@ apt-get update -qy
 apt-get install -y --upgrade python3.5 python3.5-dev g++-5 gcc-5 lcov cmake git curl
 curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | python3.5
 python3.5 -m pip install virtualenv
-curl -sL https://deb.nodesource.com/setup | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_4.x | bash -
 apt-get install -y nodejs
 npm install -g clang-format
