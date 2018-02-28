bb9521f0e42bc815e7b594589e9fa5ad5aa838ed
Thomas Madden <tmadden@mgh.harvard.edu>
2018-02-28 16:55:21 -0500

    Fix conan-community repository URL.

--- scripts/set-up-conan.sh
+++ scripts/set-up-conan.sh
@@ -2,5 +2,5 @@
 # Install additional Conan remotes that are needed to build alia.
 echo "Setting up Conan..."
 set -x -e
-conan remote add conan-community https://api.bintray.com/conan/conan/conan-community
+conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
 conan remote add conan-transit https://api.bintray.com/conan/conan/conan-transit
