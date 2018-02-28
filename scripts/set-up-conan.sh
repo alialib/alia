d58b6e7eded8d33cd8ae6c5487df7ee5fd84ac54
Thomas Madden <tmadden@mgh.harvard.edu>
2018-02-28 16:14:00 -0500

    Fix Conan sources.

--- scripts/set-up-conan.sh
+++ scripts/set-up-conan.sh
@@ -2,5 +2,4 @@
 # Install additional Conan remotes that are needed to build alia.
 echo "Setting up Conan..."
 set -x -e
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
+conan remote add conan-transit https://api.bintray.com/conan/conan/conan-transit
