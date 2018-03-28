281d4129d7aeef3284e90a94b6813845f844dd28
Thomas Madden <tmadden@mgh.harvard.edu>
2018-03-28 17:56:28 -0400

    Add better unit testing for static component collections.

--- tests/static_component_collections.cpp
+++ tests/static_component_collections.cpp
@@ -9,27 +9,54 @@ using namespace alia;
 #error ALIA_STATIC_COMPONENT_CHECKING not defined
 #endif
 
-// Our test components...
+// Define some arbitrary tag and data types.
 struct foo_tag
 {
 };
 struct foo
 {
+    bool b = false;
 };
 struct bar_tag
 {
 };
 struct bar
 {
+    int i = 0;
 };
 struct zap_tag
 {
 };
 struct zap
 {
+    double d = 0;
 };
 
-// Define some arbitrary component collections.
+// Test the underlying mechanics of adding and removing components from lists.
+namespace list_tests {
+using namespace detail;
+using list_empty = component_list<>;
+static_assert(!component_list_contains_tag<list_empty, foo_tag>::value, "");
+using list_b = add_component_to_list<list_empty, component<bar_tag, bar>>::type;
+static_assert(!component_list_contains_tag<list_b, foo_tag>::value, "");
+static_assert(component_list_contains_tag<list_b, bar_tag>::value, "");
+using list_fb = add_component_to_list<list_b, component<foo_tag, foo>>::type;
+static_assert(component_list_contains_tag<list_fb, foo_tag>::value, "");
+static_assert(component_list_contains_tag<list_fb, bar_tag>::value, "");
+using list_f = remove_component_from_list<list_fb, bar_tag>::type;
+static_assert(component_list_contains_tag<list_f, foo_tag>::value, "");
+static_assert(!component_list_contains_tag<list_f, bar_tag>::value, "");
+using list_zfb = add_component_to_list<list_fb, component<zap_tag, zap>>::type;
+static_assert(component_list_contains_tag<list_zfb, foo_tag>::value, "");
+static_assert(component_list_contains_tag<list_zfb, bar_tag>::value, "");
+static_assert(component_list_contains_tag<list_zfb, zap_tag>::value, "");
+using list_zb = remove_component_from_list<list_zfb, foo_tag>::type;
+static_assert(!component_list_contains_tag<list_zb, foo_tag>::value, "");
+static_assert(component_list_contains_tag<list_zb, bar_tag>::value, "");
+static_assert(component_list_contains_tag<list_zb, zap_tag>::value, "");
+} // namespace list_tests
+
+// Define some arbitrary component collection types.
 using storage_type = generic_component_storage<any>;
 using cc_empty = empty_component_collection<storage_type>;
 using cc_b = add_component_type_t<cc_empty, bar_tag, bar>;
@@ -37,40 +64,66 @@ using cc_fb = add_component_type_t<cc_b, foo_tag, foo>;
 using cc_z = add_component_type_t<cc_empty, zap_tag, zap>;
 using cc_bz = add_component_type_t<cc_z, bar_tag, bar>;
 using cc_fbz = add_component_type_t<cc_bz, foo_tag, foo>;
+using cc_fz = remove_component_type_t<cc_fbz, bar_tag>;
+using cc_f = add_component_type_t<cc_empty, foo_tag, foo>;
 
+// Test the underlying type functions on component collections.
+namespace cc_type_tests {
+using namespace detail;
+// detail::component_collection_contains_tag tests
+static_assert(component_collection_contains_tag<cc_fb, foo_tag>::value, "");
+static_assert(component_collection_contains_tag<cc_fb, bar_tag>::value, "");
+static_assert(!component_collection_contains_tag<cc_fb, foo>::value, "");
+static_assert(!component_collection_contains_tag<cc_fb, zap_tag>::value, "");
+// detail::collection_contains_component tests
 static_assert(
-    detail::component_collection_contains_tag<cc_fb, foo_tag>::value, "");
+    collection_contains_component<cc_fb, component<foo_tag, foo>>::value, "");
 static_assert(
-    detail::component_collection_contains_tag<cc_fb, bar_tag>::value, "");
+    collection_contains_component<cc_fb, component<bar_tag, bar>>::value, "");
 static_assert(
-    !detail::component_collection_contains_tag<cc_fb, foo>::value, "");
+    !collection_contains_component<cc_fb, component<zap_tag, zap>>::value, "");
 static_assert(
-    !detail::component_collection_contains_tag<cc_fb, zap_tag>::value, "");
+    collection_contains_component<cc_fz, component<foo_tag, foo>>::value, "");
+static_assert(
+    !collection_contains_component<cc_fz, component<bar_tag, bar>>::value, "");
+static_assert(
+    collection_contains_component<cc_fz, component<zap_tag, zap>>::value, "");
+// detail::component_collection_is_convertible tests
+static_assert(component_collection_is_convertible<cc_fb, cc_fb>::value, "");
+static_assert(component_collection_is_convertible<cc_b, cc_b>::value, "");
+static_assert(component_collection_is_convertible<cc_fbz, cc_fbz>::value, "");
+static_assert(component_collection_is_convertible<cc_fb, cc_b>::value, "");
+static_assert(!component_collection_is_convertible<cc_b, cc_fb>::value, "");
+static_assert(component_collection_is_convertible<cc_fbz, cc_fb>::value, "");
+static_assert(!component_collection_is_convertible<cc_fb, cc_fbz>::value, "");
+static_assert(component_collection_is_convertible<cc_fbz, cc_b>::value, "");
+static_assert(!component_collection_is_convertible<cc_b, cc_fbz>::value, "");
+} // namespace cc_type_tests
 
-static_assert(
-    detail::component_collection_is_convertible<cc_fb, cc_fb>::value, "");
-static_assert(
-    detail::component_collection_is_convertible<cc_b, cc_b>::value, "");
-static_assert(
-    detail::component_collection_is_convertible<cc_fbz, cc_fbz>::value, "");
-static_assert(
-    detail::component_collection_is_convertible<cc_fb, cc_b>::value, "");
-static_assert(
-    !detail::component_collection_is_convertible<cc_b, cc_fb>::value, "");
-static_assert(
-    detail::component_collection_is_convertible<cc_fbz, cc_fb>::value, "");
-static_assert(
-    !detail::component_collection_is_convertible<cc_fb, cc_fbz>::value, "");
-static_assert(
-    detail::component_collection_is_convertible<cc_fbz, cc_b>::value, "");
-static_assert(
-    !detail::component_collection_is_convertible<cc_b, cc_fbz>::value, "");
-
-TEST_CASE("component_collections", "[component_collections]")
+TEST_CASE("static component_collection conversions", "[component_collections]")
 {
     storage_type storage;
     cc_fb mc_fb(&storage);
     REQUIRE(mc_fb.storage == &storage);
     cc_b mc_b(mc_fb);
     REQUIRE(mc_b.storage == &storage);
+}
+
+TEST_CASE("static component access", "[component_collections]")
+{
+    storage_type storage_empty;
+    cc_empty mc_empty(&storage_empty);
+
+    storage_type storage_b;
+    cc_b mc_b = add_component<bar_tag>(&storage_b, mc_empty, bar{1});
+    REQUIRE(any_cast<bar>(get_component<bar_tag>(mc_b))->i == 1);
+
+    storage_type storage_fb;
+    cc_fb mc_fb = add_component<foo_tag>(&storage_fb, mc_b, foo());
+    REQUIRE(any_cast<bar>(get_component<bar_tag>(mc_fb))->i == 1);
+    REQUIRE(any_cast<foo>(get_component<foo_tag>(mc_fb))->b == false);
+
+    storage_type storage_f;
+    cc_f mc_f = remove_component<bar_tag>(&storage_f, mc_fb);
+    REQUIRE(any_cast<foo>(get_component<foo_tag>(mc_f))->b == false);
 }
