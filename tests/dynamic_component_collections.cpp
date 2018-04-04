40786ac01d11bda1cf7509b9e2cf6d9a1b30443b
Thomas Madden <tmadden@mgh.harvard.edu>
2018-04-04 17:59:43 -0400

    Add component collection merging.

--- tests/dynamic_component_collections.cpp
+++ tests/dynamic_component_collections.cpp
@@ -44,6 +44,7 @@ using cc_bz = add_component_type_t<cc_z, bar_tag, bar>;
 using cc_fbz = add_component_type_t<cc_bz, foo_tag, foo>;
 using cc_fz = remove_component_type_t<cc_fbz, bar_tag>;
 using cc_f = add_component_type_t<cc_empty, foo_tag, foo>;
+using cc_fzb = merge_components_t<cc_fz, cc_bz>;
 
 TEST_CASE("dynamic component_collection conversions", "[component_collections]")
 {
