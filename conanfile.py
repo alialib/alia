b110d4b77be2090a99a4ce0da54825cd8532bc1b
Thomas Madden <tmadden@mgh.harvard.edu>
2018-04-19 10:06:00 -0400

    Attempt to fix Boost on AppVeyor.

--- conanfile.py
+++ conanfile.py
@@ -1,33 +1,38 @@
 from conans import ConanFile, CMake
 import os
 
+
 class CradleConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = \
-        "Boost/1.64.0@conan/stable", \
         "catch/1.5.0@TyRoXx/stable", \
         "FakeIt/master@gasuketsu/stable"
     generators = "cmake"
     default_options = \
-        "Boost:without_atomic=True", \
-        "Boost:without_chrono=True", \
-        "Boost:without_container=True", \
-        "Boost:without_context=True", \
-        "Boost:without_coroutine=True", \
-        "Boost:without_coroutine2=True", \
-        "Boost:without_graph=True", \
-        "Boost:without_graph_parallel=True", \
-        "Boost:without_log=True", \
-        "Boost:without_math=True", \
-        "Boost:without_mpi=True", \
-        "Boost:without_serialization=True", \
-        "Boost:without_signals=True", \
-        "Boost:without_test=True", \
-        "Boost:without_timer=True", \
-        "Boost:without_type_erasure=True", \
-        "Boost:without_wave=True", \
         "FakeIt:integration=catch", \
         "*:shared=False"
+    if "APPVEYOR" not in os.environ:
+        # AppVeyor provides Boost directly (and seems to have trouble building
+        # it through Conan), so omit Boost if we're running on AppVeyor.
+        requires = requires + ("Boost/1.64.0@conan/stable", )
+        default_options = default_options + \
+           ("Boost:without_atomic=True",
+            "Boost:without_chrono=True",
+            "Boost:without_container=True",
+            "Boost:without_context=True",
+            "Boost:without_coroutine=True",
+            "Boost:without_coroutine2=True",
+            "Boost:without_graph=True",
+            "Boost:without_graph_parallel=True",
+            "Boost:without_log=True",
+            "Boost:without_math=True",
+            "Boost:without_mpi=True",
+            "Boost:without_serialization=True",
+            "Boost:without_signals=True",
+            "Boost:without_test=True",
+            "Boost:without_timer=True",
+            "Boost:without_type_erasure=True",
+            "Boost:without_wave=True")
 
     def imports(self):
         dest = os.getenv("CONAN_IMPORT_PATH", "bin")
