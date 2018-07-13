87f8df521fcde10567d6d5aa00819204839fb540
Thomas Madden <tmadden@gmail.com>
2018-07-13 14:42:03 -0400

    Make captured_id more efficient and improve ID test coverage.

--- unit_tests/id.cpp
+++ unit_tests/id.cpp
@@ -1,8 +1,13 @@
 #include <alia/id.hpp>
-#include <boost/lexical_cast.hpp>
+
+#include <map>
+#include <unordered_map>
 #include <utility>
 
+#include <boost/lexical_cast.hpp>
+
 #include <catch.hpp>
+
 
 using namespace alia;
 
@@ -72,10 +77,16 @@ TEST_CASE("id_ref", "[id]")
 TEST_CASE("captured_id", "[id]")
 {
     captured_id c;
+    REQUIRE(!c.is_initialized());
+    REQUIRE(!c.matches(make_id(0)));
     c.capture(make_id(0));
+    REQUIRE(c.is_initialized());
     REQUIRE(c.matches(make_id(0)));
     REQUIRE(!c.matches(make_id(1)));
     REQUIRE(c.get() == make_id(0));
+    c.clear();
+    REQUIRE(!c.is_initialized());
+    c.capture(make_id(0));
     captured_id d;
     REQUIRE(c != d);
     d.capture(make_id(0));
@@ -118,7 +129,7 @@ TEST_CASE("combine_ids x4", "[id]")
     test_different_ids(a, b);
 }
 
-TEST_CASE("clone_into", "[id]")
+TEST_CASE("clone_into/pointer", "[id]")
 {
     id_interface* storage = 0;
     auto zero = make_id(0);
@@ -132,4 +143,56 @@ TEST_CASE("clone_into", "[id]")
     REQUIRE(*storage == abc);
     clone_into(storage, nullptr);
     REQUIRE(!storage);
+}
+
+TEST_CASE("clone_into/shared_ptr", "[id]")
+{
+    std::shared_ptr<id_interface> storage;
+    auto zero = make_id(0);
+    auto abc = make_id(string("abc"));
+    auto one = make_id(1);
+    clone_into(storage, &zero);
+    REQUIRE(*storage == zero);
+    clone_into(storage, &one);
+    REQUIRE(*storage == one);
+    clone_into(storage, &abc);
+    REQUIRE(*storage == abc);
+    clone_into(storage, nullptr);
+    REQUIRE(!storage);
+}
+
+TEST_CASE("map of IDs", "[id]")
+{
+    auto zero = make_id(0);
+    auto abc = make_id(string("abc"));
+    auto one = make_id(1);
+    auto another_one = make_id(1);
+
+    std::map<id_interface const*,int,id_interface_pointer_less_than_test> m;
+    m[&zero] = 0;
+    m[&abc] = 123;
+    REQUIRE(m.at(&zero) == 0);
+    REQUIRE(m.at(&abc) == 123);
+    REQUIRE(m.find(&one) == m.end());
+    m[&one] = 1;
+    REQUIRE(m.at(&one) == 1);
+    REQUIRE(m.at(&another_one) == 1);
+}
+
+TEST_CASE("unordered_map of IDs", "[id]")
+{
+    auto zero = make_id(0);
+    auto abc = make_id(string("abc"));
+    auto one = make_id(1);
+    auto another_one = make_id(1);
+
+    std::unordered_map<id_interface const*,int,id_interface_pointer_hash,id_interface_pointer_equality_test> m;
+    m[&zero] = 0;
+    m[&abc] = 123;
+    REQUIRE(m.at(&zero) == 0);
+    REQUIRE(m.at(&abc) == 123);
+    REQUIRE(m.find(&one) == m.end());
+    m[&one] = 1;
+    REQUIRE(m.at(&one) == 1);
+    REQUIRE(m.at(&another_one) == 1);
 }
