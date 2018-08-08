b1eedf913f9841787dda3b24b70804197dcc826a
Thomas Madden <tmadden@mgh.harvard.edu>
2018-08-08 17:25:59 -0400

    Update numeric signal utilities so all arguments are signals.

--- src/alia/signals/numeric.hpp
+++ src/alia/signals/numeric.hpp
@@ -10,143 +10,136 @@
 
 namespace alia {
 
-// scale(a, factor) creates a new signal that presents a scaled view of a,
-// where a is a signal to a numeric value.
-template<class Wrapped>
-struct scaling_signal_wrapper : regular_signal<
-                                    scaling_signal_wrapper<Wrapped>,
-                                    typename Wrapped::value_type,
-                                    typename Wrapped::direction_tag>
+// scale(n, factor) creates a new signal that presents a scaled view of :n,
+// where :n and :factor are both numeric signals.
+template<class N, class Factor>
+struct scaled_signal : regular_signal<
+                           scaled_signal<N, Factor>,
+                           typename N::value_type,
+                           typename N::direction_tag>
 {
-    typedef typename Wrapped::value_type wrapped_value_type;
-    scaling_signal_wrapper(Wrapped wrapped, wrapped_value_type scale_factor)
-        : wrapped_(wrapped), scale_factor_(scale_factor)
+    scaled_signal(N n, Factor scale_factor) : n_(n), scale_factor_(scale_factor)
     {
     }
     bool
     is_readable() const
     {
-        return wrapped_.is_readable();
+        return n_.is_readable() && scale_factor_.is_readable();
     }
-    wrapped_value_type const&
+    typename N::value_type const&
     read() const
     {
         return lazy_reader_.read(
-            [&] { return wrapped_.read() * scale_factor_; });
+            [&] { return n_.read() * scale_factor_.read(); });
     }
     bool
     is_writable() const
     {
-        return wrapped_.is_writable();
+        return n_.is_writable() && scale_factor_.is_readable();
     }
     void
-    write(wrapped_value_type const& value) const
+    write(typename N::value_type const& value) const
     {
-        wrapped_.write(value / scale_factor_);
+        n_.write(value / scale_factor_.read());
     }
 
  private:
-    Wrapped wrapped_;
-    wrapped_value_type scale_factor_;
-    lazy_reader<wrapped_value_type> lazy_reader_;
+    N n_;
+    Factor scale_factor_;
+    lazy_reader<typename N::value_type> lazy_reader_;
 };
-template<class Wrapped>
-scaling_signal_wrapper<Wrapped>
-scale(Wrapped const& wrapped, typename Wrapped::value_type scale_factor)
+template<class N, class Factor>
+scaled_signal<N, Factor>
+scale(N const& n, Factor const& scale_factor)
 {
-    return scaling_signal_wrapper<Wrapped>(wrapped, scale_factor);
+    return scaled_signal<N, Factor>(n, scale_factor);
 }
 
-// offset(a, offset) presents an offset view of a, where a is a signal
-// to a numeric value.
-template<class Wrapped>
-struct offset_signal_wrapper : regular_signal<
-                                   offset_signal_wrapper<Wrapped>,
-                                   typename Wrapped::value_type,
-                                   typename Wrapped::direction_tag>
+// offset(n, offset) presents an offset view of :n.
+template<class N, class Offset>
+struct offset_signal : regular_signal<
+                           offset_signal<N, Offset>,
+                           typename N::value_type,
+                           typename N::direction_tag>
 {
-    typedef typename Wrapped::value_type wrapped_value_type;
-    offset_signal_wrapper(Wrapped wrapped, typename Wrapped::value_type offset)
-        : wrapped_(wrapped), offset_(offset)
+    offset_signal(N n, Offset offset) : n_(n), offset_(offset)
     {
     }
     bool
     is_readable() const
     {
-        return wrapped_.is_readable();
+        return n_.is_readable() && offset_.is_readable();
     }
-    wrapped_value_type const&
+    typename N::value_type const&
     read() const
     {
-        return lazy_reader_.read([&] { return wrapped_.read() + offset_; });
+        return lazy_reader_.read([&] { return n_.read() + offset_.read(); });
     }
     bool
     is_writable() const
     {
-        return wrapped_.is_writable();
+        return n_.is_writable() && offset_.is_readable();
     }
     void
-    write(typename Wrapped::value_type const& value) const
+    write(typename N::value_type const& value) const
     {
-        wrapped_.write(value - offset_);
+        n_.write(value - offset_.read());
     }
 
  private:
-    Wrapped wrapped_;
-    wrapped_value_type offset_;
-    lazy_reader<wrapped_value_type> lazy_reader_;
+    N n_;
+    Offset offset_;
+    lazy_reader<typename N::value_type> lazy_reader_;
 };
-template<class Wrapped>
-offset_signal_wrapper<Wrapped>
-offset(Wrapped const& wrapped, typename Wrapped::value_type offset)
+template<class N, class Offset>
+offset_signal<N, Offset>
+offset(N const& n, Offset const& offset)
 {
-    return offset_signal_wrapper<Wrapped>(wrapped, offset);
+    return offset_signal<N, Offset>(n, offset);
 }
 
-// round_signal_writes(signal, step) yields a wrapper which rounds any writes to
-// :signal so that values are always a multiple of :step.
-template<class Wrapped>
+// round_signal_writes(n, step) yields a wrapper which rounds any writes to
+// :n so that values are always a multiple of :step.
+template<class N, class Step>
 struct rounding_signal_wrapper : regular_signal<
-                                     rounding_signal_wrapper<Wrapped>,
-                                     typename Wrapped::value_type,
-                                     typename Wrapped::direction_tag>
+                                     rounding_signal_wrapper<N, Step>,
+                                     typename N::value_type,
+                                     typename N::direction_tag>
 {
-    rounding_signal_wrapper(Wrapped wrapped, typename Wrapped::value_type step)
-        : wrapped_(wrapped), step_(step)
+    rounding_signal_wrapper(N n, Step step) : n_(n), step_(step)
     {
     }
     bool
     is_readable() const
     {
-        return wrapped_.is_readable();
+        return n_.is_readable();
     }
-    typename Wrapped::value_type const&
+    typename N::value_type const&
     read() const
     {
-        return wrapped_.read();
+        return n_.read();
     }
     bool
     is_writable() const
     {
-        return wrapped_.is_writable();
+        return n_.is_writable() && step_.is_readable();
     }
     void
-    write(typename Wrapped::value_type const& value) const
+    write(typename N::value_type const& value) const
     {
-        wrapped_.write(
-            std::floor(value / step_ + typename Wrapped::value_type(0.5))
-            * step_);
+        typename N::value_type step = step_.read();
+        n_.write(std::floor(value / step + typename N::value_type(0.5)) * step);
     }
 
  private:
-    Wrapped wrapped_;
-    typename Wrapped::value_type step_;
+    N n_;
+    Step step_;
 };
-template<class Wrapped>
-rounding_signal_wrapper<Wrapped>
-round_signal_writes(Wrapped const& wrapped, typename Wrapped::value_type step)
+template<class N, class Step>
+rounding_signal_wrapper<N, Step>
+round_signal_writes(N const& n, Step const& step)
 {
-    return rounding_signal_wrapper<Wrapped>(wrapped, step);
+    return rounding_signal_wrapper<N, Step>(n, step);
 }
 
 } // namespace alia
