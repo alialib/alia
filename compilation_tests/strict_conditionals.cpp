9f85c13ee21cd65458c4329c696cb657f3a50f0e
Thomas Madden <tmadden@mgh.harvard.edu>
2018-08-08 16:41:26 -0400

    Add missing compilation test.

---
+++ compilation_tests/strict_conditionals.cpp
@@ -0,0 +1,16 @@
+#ifdef ALIA_TEST_COMPILATION_FAILURE
+#define ALIA_STRICT_CONDITIONALS
+#endif
+
+#include <alia/data_graph.hpp>
+
+using namespace alia;
+
+void
+f(data_traversal& ctx)
+{
+    ALIA_IF(1 > 0)
+    {
+    }
+    ALIA_END
+}
