a5b7de2413621adde37e5fc5564ff7e68fb60022
Thomas Madden <tmadden@mgh.harvard.edu>
2018-10-10 14:46:12 -0400

    Add bincrafters Conan source.

--- scripts/set-up-conan.bat
+++ scripts/set-up-conan.bat
@@ -1,5 +1,4 @@
 :: Install additional Conan remotes that are needed to build alia.
 @echo off
 echo "Setting up Conan..."
-conan remote add conan-community https://api.bintray.com/conan/conan-community/conan || exit /b
-conan remote add conan-transit https://api.bintray.com/conan/conan/conan-transit || exit /b
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan || exit /b
