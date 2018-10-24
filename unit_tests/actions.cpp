72cb5cbd0f0ce2c852f6db66e62b9e7d0a7b9bca
Thomas Madden <tmadden@mgh.harvard.edu>
2018-10-24 15:56:23 -0400

    Add lambda actions.

--- unit_tests/actions.cpp
+++ unit_tests/actions.cpp
@@ -105,3 +105,19 @@ TEST_CASE("push_back action", "[actions]")
         REQUIRE(!a.is_ready());
     }
 }
+
+TEST_CASE("lambda actions", "[actions]")
+{
+    int x = 0;
+    auto a = lambda_action(always_ready, [&](int y, int z) { x = y + z; });
+    perform_action(a, 1, 2);
+    REQUIRE(x == 3);
+
+    bool ready = false;
+    auto b = lambda_action([&]() { return ready; }, [&](int y) { x += y; });
+    REQUIRE(!b.is_ready());
+    ready = true;
+    REQUIRE(b.is_ready());
+    perform_action(b, 1);
+    REQUIRE(x == 4);
+}
