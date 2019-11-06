663f6862b0bf0920df87a6d509347abba9e2e272
Thomas Madden <tmadden@mgh.harvard.edu>
2019-11-06 17:48:33 -0500

    Add signal/action operators for +=, ++, etc.

--- unit_tests/flow/actions.cpp
+++ unit_tests/flow/actions.cpp
@@ -19,6 +19,61 @@ TEST_CASE("copy actions", "[actions]")
     REQUIRE(x == 2);
 }
 
+#define TEST_COMPOUND_ASSIGNMENT_OPERATOR(op, normal_form)                     \
+    {                                                                          \
+        int x = 21;                                                            \
+        REQUIRE(!(empty<int>() op empty<int>()).is_ready());                   \
+        REQUIRE(!(direct(x) op empty<int>()).is_ready());                      \
+        REQUIRE(!(empty<int>() op direct(x)).is_ready());                      \
+        auto a = direct(x) op value(7);                                        \
+        REQUIRE(a.is_ready());                                                 \
+        REQUIRE(x == 21);                                                      \
+        perform_action(a);                                                     \
+        REQUIRE(x == (21 normal_form 7));                                      \
+    }
+
+TEST_CASE("compound assignment action operators", "[actions]")
+{
+    TEST_COMPOUND_ASSIGNMENT_OPERATOR(+=, +)
+    TEST_COMPOUND_ASSIGNMENT_OPERATOR(-=, -)
+    TEST_COMPOUND_ASSIGNMENT_OPERATOR(*=, *)
+    TEST_COMPOUND_ASSIGNMENT_OPERATOR(/=, /)
+    TEST_COMPOUND_ASSIGNMENT_OPERATOR(^=, ^)
+    TEST_COMPOUND_ASSIGNMENT_OPERATOR(%=, %)
+    TEST_COMPOUND_ASSIGNMENT_OPERATOR(&=, &)
+    TEST_COMPOUND_ASSIGNMENT_OPERATOR(|=, |)
+}
+
+#undef TEST_BY_ONE_OPERATOR
+
+#define TEST_BY_ONE_OPERATOR(op, normal_form)                                  \
+    {                                                                          \
+        int x = 21;                                                            \
+        REQUIRE(!(empty<int>() op).is_ready());                                \
+        REQUIRE(!(op empty<int>()).is_ready());                                \
+        {                                                                      \
+            auto a = direct(x) op;                                             \
+            REQUIRE(a.is_ready());                                             \
+            REQUIRE(x == 21);                                                  \
+            perform_action(a);                                                 \
+            REQUIRE(x == (21 normal_form 1));                                  \
+        }                                                                      \
+        {                                                                      \
+            auto a = op direct(x);                                             \
+            REQUIRE(a.is_ready());                                             \
+            perform_action(a);                                                 \
+            REQUIRE(x == (21 normal_form 2));                                  \
+        }                                                                      \
+    }
+
+TEST_CASE("increment/decrement operators", "[actions]")
+{
+    TEST_BY_ONE_OPERATOR(++, +)
+    TEST_BY_ONE_OPERATOR(--, -)
+}
+
+#undef TEST_COMPOUND_ASSIGNMENT_OPERATOR
+
 TEST_CASE("sequenced actions", "[actions]")
 {
     int x = 1, y = 2;
