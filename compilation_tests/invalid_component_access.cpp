16e7eaaa587e96e219a719c45343c86f4a63a61f
Thomas Madden <tmadden@gmail.com>
2019-11-25 18:12:01 -0500

    Fix a broken compilation test.

--- compilation_tests/invalid_component_access.cpp
+++ compilation_tests/invalid_component_access.cpp
@@ -4,20 +4,16 @@
 
 using namespace alia;
 
-struct foo_tag
-{
-};
 struct foo
 {
     bool b = false;
 };
-struct bar_tag
-{
-};
+ALIA_DEFINE_COMPONENT_TYPE(foo_tag, foo)
 struct bar
 {
     int i = 0;
 };
+ALIA_DEFINE_COMPONENT_TYPE(bar_tag, bar)
 
 using storage_type = generic_component_storage<boost::any>;
 using cc_empty = empty_component_collection<storage_type>;
@@ -25,17 +21,11 @@ using cc_empty = empty_component_collection<storage_type>;
 void
 f()
 {
-    storage_type storage_empty;
-    cc_empty mc_empty(&storage_empty);
-
-    storage_type storage_b;
-    auto mc_b = add_component<bar_tag>(&storage_b, mc_empty, bar());
-
-    storage_type storage_fb;
-    auto mc_fb = add_component<foo_tag>(&storage_fb, mc_b, foo());
-
-    storage_type storage_f;
-    auto mc_f = remove_component<bar_tag>(&storage_f, mc_fb);
+    storage_type storage;
+    cc_empty mc_empty(&storage);
+    auto mc_b = add_component<bar_tag>(mc_empty, bar());
+    auto mc_fb = add_component<foo_tag>(mc_b, foo());
+    auto mc_f = remove_component<bar_tag>(mc_fb);
     get_component<foo_tag>(mc_f);
 #ifdef ALIA_TEST_COMPILATION_FAILURE
     get_component<bar_tag>(mc_f);
