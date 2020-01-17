1e0b1175b5658271e6efa100e5900aa2041a87ac
Thomas Madden <tmadden@mgh.harvard.edu>
2020-01-17 18:00:03 -0500

    Eliminate Boost as a dependency for testing.

--- conanfile.py
+++ conanfile.py
@@ -8,12 +8,6 @@ class AliaConan(ConanFile):
     generators = "cmake"
     default_options = "*:shared=False"
 
-    # AppVeyor provides Boost directly (and seems to have trouble building it
-    # through Conan), so omit Boost if we're running on AppVeyor.
-    if "APPVEYOR" not in os.environ:
-        requires = requires + ("boost_any/1.68.0@bincrafters/stable",
-                               "boost_lexical_cast/1.68.0@bincrafters/stable")
-
     def imports(self):
         dest = os.getenv("CONAN_IMPORT_PATH", "bin")
         self.copy("*.dll", dst=dest, src="bin")
