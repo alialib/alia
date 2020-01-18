0765e141f293497dd0cce76c886960536692dfd2
Thomas Madden <tmadden@gmail.com>
2020-01-18 18:35:20 -0500

    Clean up component tests.

--- compilation_tests/invalid_component_access.cpp
+++ compilation_tests/invalid_component_access.cpp
@@ -4,18 +4,10 @@
 
 using namespace alia;
 
-struct foo
-{
-    bool b = false;
-};
-ALIA_DEFINE_COMPONENT_TYPE(foo_tag, foo)
-struct bar
-{
-    int i = 0;
-};
-ALIA_DEFINE_COMPONENT_TYPE(bar_tag, bar)
+ALIA_DEFINE_COMPONENT_TYPE(foo_tag, int)
+ALIA_DEFINE_COMPONENT_TYPE(bar_tag, int)
 
-using storage_type = generic_component_storage<any_value>;
+using storage_type = generic_component_storage<int>;
 using cc_empty = empty_component_collection<storage_type>;
 
 void
@@ -23,8 +15,8 @@ f()
 {
     storage_type storage;
     cc_empty mc_empty(&storage);
-    auto mc_b = add_component<bar_tag>(mc_empty, bar());
-    auto mc_fb = add_component<foo_tag>(mc_b, foo());
+    auto mc_b = add_component<bar_tag>(mc_empty, 0);
+    auto mc_fb = add_component<foo_tag>(mc_b, 1);
     auto mc_f = remove_component<bar_tag>(mc_fb);
     get_component<foo_tag>(mc_f);
 #ifdef ALIA_TEST_COMPILATION_FAILURE
