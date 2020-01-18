06b11413f6c4369acd69c2c8861efbea87ee148d
Thomas Madden <tmadden@gmail.com>
2020-01-18 00:20:24 -0500

    Add any_value tests.

---
+++ unit_tests/components/storage.cpp
@@ -0,0 +1,40 @@
+#include <alia/components/storage.hpp>
+
+#include <catch.hpp>
+
+using namespace alia;
+
+TEST_CASE("any_value basics", "[components][storage]")
+{
+    any_value a(int(17));
+    REQUIRE(any_cast<int>(&a));
+    REQUIRE(*any_cast<int>(&a) == 17);
+    REQUIRE(!any_cast<bool>(&a));
+
+    any_value b(std::move(a));
+    REQUIRE(*any_cast<int>(&b) == 17);
+
+    any_value const c = b;
+    REQUIRE(*any_cast<int>(&c) == 17);
+
+    any_value d;
+    d = 12;
+    REQUIRE(*any_cast<int>(&d) == 12);
+
+    any_value e;
+    e = b;
+    REQUIRE(*any_cast<int>(&e) == 17);
+
+    any_value f;
+    f = std::move(e);
+    REQUIRE(*any_cast<int>(&f) == 17);
+
+    any_value g;
+    std::string abc("abc");
+    g = std::move(abc);
+    REQUIRE(*any_cast<std::string>(&g) == "abc");
+
+    swap(f, g);
+    REQUIRE(*any_cast<int>(&g) == 17);
+    REQUIRE(*any_cast<std::string>(&f) == "abc");
+}
