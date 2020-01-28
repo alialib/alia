3de77cb56ed67cdde1adc7815d9b90eadc677433
Thomas Madden <tmadden@gmail.com>
2020-01-27 20:37:32 -0500

    Add an option for strict operators.

---
+++ compilation_tests/strict_compound_operator.cpp
@@ -0,0 +1,13 @@
+#ifdef ALIA_TEST_COMPILATION_FAILURE
+#define ALIA_STRICT_OPERATORS
+#endif
+
+#include <alia/signals/operators.hpp>
+
+using namespace alia;
+
+void
+f()
+{
+    auto x = value(1) + 2;
+}
