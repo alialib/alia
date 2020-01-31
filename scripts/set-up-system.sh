536defd2f605d9beec6154231f88ac9169ec53de
Thomas Madden <tmadden@gmail.com>
2020-01-31 12:33:39 -0500

    Clean up Travis jobs.

--- scripts/set-up-system.sh
+++ scripts/set-up-system.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-# Set up a fresh Ubuntu installation so that it can build alia.
+# Set up a fresh Ubuntu Xenial installation so that it can build alia.
 # This should be run with sudo.
 echo "Setting up system..."
 set -x -e
-apt-get update -qy
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-get update -qy
-apt-get install -y --upgrade --allow-unauthenticated g++-5 gcc-5 lcov cmake git curl clang-4.0 llvm-4.0
-pip install virtualenv
+apt-get install -y --upgrade g++-5 gcc-5 lcov cmake git curl clang-4.0 llvm-4.0
