4caa685373a6a4cc85307eab04adf7e3c122943f
Thomas Madden <tmadden@gmail.com>
2020-02-05 11:57:21 -0500

    WIP

--- scripts/set-up-travis.sh
+++ scripts/set-up-travis.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Set up Travis for a "normal" CI build.
-# This should be run with sudo.
 set -x -e
-scripts/set-up-system.sh
-sudo -u $SUDO_USER bash -c 'pip install -r requirements.txt'
-sudo -u $SUDO_USER bash scripts/set-up-conan.sh
+pip install -r requirements.txt
+scripts/set-up-conan.sh
