af0cd114bfeece954a63b29d6514d2044072d26a
Thomas Madden <tmadden@gmail.com>
2020-02-21 22:43:23 -0500

    Add lots of action documentation.

--- examples/asm-dom/demos/actions.cpp
+++ examples/asm-dom/demos/actions.cpp
@@ -110,3 +110,67 @@ init_action_latching(std::string dom_id)
 }
 
 static demo action_latching("action-latching", init_action_latching);
+
+void
+do_action_binding(dom::context ctx, bidirectional<int> duration)
+{
+    // clang-format off
+/// [action-binding]
+animation_timer timer(ctx);
+
+alia_if(timer.is_active())
+{
+    do_text(ctx, printf(ctx, "%d ms left.", timer.ticks_left()));
+}
+alia_else
+{
+    do_text(ctx, "The timer is stopped.");
+}
+alia_end
+
+do_text(ctx, "Enter a duration in milliseconds:");
+do_input(ctx, duration);
+do_button(ctx, "Start", timer.start() <<= duration);
+/// [action-binding]
+    // clang-format on
+}
+
+void
+init_action_binding(std::string dom_id)
+{
+    static alia::system the_system;
+    static dom::system the_dom;
+
+    initialize(the_dom, the_system, dom_id, [](dom::context ctx) {
+        do_action_binding(ctx, get_state(ctx, empty<int>()));
+    });
+}
+
+static demo action_binding("action-binding", init_action_binding);
+
+void
+do_lambda_action_demo(dom::context ctx, bidirectional<std::string> message)
+{
+    // clang-format off
+/// [lambda-action-demo]
+do_text(ctx, "Enter a message for your browser's console:");
+do_input(ctx, message);
+auto sender = lambda_action(
+    [](std::string message) { std::cout << message << std::endl; });
+do_button(ctx, "Send", sender <<= message);
+/// [lambda-action-demo]
+    // clang-format on
+}
+
+void
+init_lambda_action_demo(std::string dom_id)
+{
+    static alia::system the_system;
+    static dom::system the_dom;
+
+    initialize(the_dom, the_system, dom_id, [](dom::context ctx) {
+        do_lambda_action_demo(ctx, get_state(ctx, std::string()));
+    });
+}
+
+static demo lambda_action_demo("lambda-action-demo", init_lambda_action_demo);
