9dc60a39c1ab6748abc163949c93566d098d3f1f
Thomas Madden <tmadden@gmail.com>
2020-02-22 16:48:56 -0500

    Fix code styling scripts.

--- scripts/apply-code-styling.bat
+++
@@ -1,8 +0,0 @@
-:: Apply our .clang-format styling to all C++ source files.
-:: Note that this should be run from the root directory.
-@echo off
-setlocal enabledelayedexpansion
-for /F usebackq^ tokens^=*^ delims^=^ eol^= %%f in (`dir /s /b *.hpp *.cpp *.ipp`) do (
-    clang-format -style=file -i %%f
-)
-endlocal
--- scripts/apply-code-styling.sh
+++ scripts/apply-code-styling.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Apply our .clang-format styling to all C++ source files.
 # Note that this should be run from the root directory.
-find . -name *.[chi]pp | xargs -n1 clang-format -i -style=file
+find src unit_tests examples -path examples/asm-dom/build -prune -o \
+    -name *.[chi]pp | xargs -n1 clang-format -i -style=file
