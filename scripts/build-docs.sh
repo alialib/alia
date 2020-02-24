151c616cb9c8c045763648f240fc9c667fee344a
Thomas Madden <tmadden@gmail.com>
2020-02-23 19:38:28 -0500

    Work around docsify embedding issue.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -9,5 +9,6 @@ rm -rf *
 cp ../docs/* .
 unzip favicon.zip && rm *.zip
 cp ../examples/asm-dom/build/*.js ../examples/asm-dom/build/*.wasm .
-cp ../examples/asm-dom/demos/* .
 dot -Tsvg -o data-graph.svg data-graph.dot && rm data-graph.dot
+cd ..
+python scripts/process-docs.py
