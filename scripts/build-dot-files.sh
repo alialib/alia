3799c700ab59baf5ef64b17064002dd08877dcbc
Thomas Madden <tmadden@mgh.harvard.edu>
2020-03-06 18:10:58 -0500

    Add reactive flow diagrams.

---
+++ scripts/build-dot-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+if [ $(basename $(pwd)) != "www" ]; then
+    echo "This script must be run from the www directory!"
+    exit 1
+fi
+dot -Tsvg -o data-graph.svg data-graph.dot
+dot -Tsvg -o alia-flow.svg reactive-flow.dot
+sed -i 's/label="alia controller"; style=dashed;/style=invis;/' reactive-flow.dot
+dot -Tsvg -o reactive-flow.svg reactive-flow.dot
+rm *.dot
