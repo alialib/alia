b93fb89ec9db1d5247897f6ab44dce6045b50043
Thomas Madden <tmadden@gmail.com>
2020-04-06 10:22:10 -0400

    Add function_view.

---
+++ compilation_tests/bad_function_view_application.cpp
@@ -0,0 +1,25 @@
+#include <alia/common.hpp>
+
+using namespace alia;
+
+int
+invoke_with_bool(function_view<int(bool)> a)
+{
+    return a(false);
+}
+
+int
+invoke_with_string(function_view<int(std::string)> a)
+{
+    return a(std::string("foo"));
+}
+
+void
+f()
+{
+    invoke_with_bool([](bool x) { return x ? 3 : 1; });
+    invoke_with_string([](std::string x) { return x.empty() ? 3 : 1; });
+#ifdef ALIA_TEST_COMPILATION_FAILURE
+    invoke_with_bool([](std::string x) { return x.empty() ? 3 : 1; });
+#endif
+}
