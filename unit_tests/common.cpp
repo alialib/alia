ec0d9b18a6119e05d8b13fade713da0ff365540d
Thomas Madden <tmadden@gmail.com>
2020-04-06 10:22:10 -0400

    Add function_view.

--- unit_tests/common.cpp
+++ unit_tests/common.cpp
@@ -11,3 +11,21 @@ TEST_CASE("exception", "[common]")
     e.add_context("in here");
     REQUIRE(e.what() == std::string("just a test\nin here"));
 }
+
+int
+invoke_function_views(function_view<int(bool)> a, function_view<int(int)> b)
+{
+    return a(true) + b(12);
+}
+
+TEST_CASE("function_view", "[common]")
+{
+    REQUIRE(
+        invoke_function_views(
+            [](bool x) { return x ? 1 : 2; }, [](int x) { return x / 2; })
+        == 7);
+    REQUIRE(
+        invoke_function_views(
+            [](bool x) { return x ? 3 : 0; }, [](int x) { return x * 2; })
+        == 27);
+}
