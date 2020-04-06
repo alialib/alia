fdf737ae2047e7bbbf8b2c4d1e582f4bfd636e59
Thomas Madden <tmadden@gmail.com>
2020-04-06 10:22:10 -0400

    Add function_view.

--- src/alia/common.hpp
+++ src/alia/common.hpp
@@ -89,6 +89,36 @@ using void_t = typename make_void<Ts...>::type;
 #define alia_lambdify(f) ALIA_LAMBDIFY(f)
 #endif
 
+// function_view is the non-owning equivalent of std::function.
+template<class Signature>
+class function_view;
+template<class Return, class... Args>
+class function_view<Return(Args...)> final
+{
+ private:
+    using signature_type = Return(void*, Args...);
+
+    void* _ptr;
+    Return (*_erased_fn)(void*, Args...);
+
+ public:
+    template<typename T>
+    function_view(T&& x) noexcept : _ptr{(void*) std::addressof(x)}
+    {
+        _erased_fn = [](void* ptr, Args... xs) -> Return {
+            return (*reinterpret_cast<std::add_pointer_t<T>>(ptr))(
+                std::forward<Args>(xs)...);
+        };
+    }
+
+    decltype(auto)
+    operator()(Args... xs) const
+        noexcept(noexcept(_erased_fn(_ptr, std::forward<Args>(xs)...)))
+    {
+        return _erased_fn(_ptr, std::forward<Args>(xs)...);
+    }
+};
+
 } // namespace alia
 
 #endif
