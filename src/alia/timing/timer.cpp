aaf189489d05e19349d3191c1b03d4d39d0e13e5
Thomas Madden <tmadden@gmail.com>
2020-04-10 12:11:59 -0400

    Simplify scheduler requirements.

--- src/alia/timing/timer.cpp
+++ src/alia/timing/timer.cpp
@@ -11,32 +11,38 @@ schedule_timer_event(
         make_routable_node_id(ctx, id), trigger_time);
 }
 
-void
-start_timer(dataless_context ctx, node_id id, millisecond_count duration)
-{
-    auto now = ctx.get<timing_tag>().tick_counter;
-    schedule_timer_event(ctx, id, now + duration);
-}
-
 bool
-detect_timer_event(dataless_context ctx, node_id id)
+detect_timer_event(dataless_context ctx, timer_data& data)
 {
     timer_event* event;
-    return detect_targeted_event(ctx, id, &event);
+    return detect_targeted_event(ctx, &data, &event)
+           && event->trigger_time == data.expected_trigger_time;
 }
 
 void
-restart_timer(dataless_context ctx, node_id id, millisecond_count duration)
+start_timer(dataless_context ctx, timer_data& data, millisecond_count duration)
+{
+    auto now = ctx.get<timing_tag>().tick_counter;
+    auto trigger_time = now + duration;
+    data.expected_trigger_time = trigger_time;
+    schedule_timer_event(ctx, &data, trigger_time);
+}
+
+void
+restart_timer(
+    dataless_context ctx, timer_data& data, millisecond_count duration)
 {
     timer_event* event;
     assert(detect_event(ctx, &event));
-    schedule_timer_event(ctx, id, event->trigger_time + duration);
+    auto trigger_time = event->trigger_time + duration;
+    data.expected_trigger_time = trigger_time;
+    schedule_timer_event(ctx, &data, trigger_time);
 }
 
 void
 raw_timer::update()
 {
-    triggered_ = data_->active && detect_timer_event(ctx_, data_);
+    triggered_ = data_->active && detect_timer_event(ctx_, *data_);
     if (triggered_)
         data_->active = false;
 }
@@ -45,9 +51,9 @@ void
 raw_timer::start(unsigned duration)
 {
     if (triggered_)
-        restart_timer(ctx_, data_, duration);
+        restart_timer(ctx_, *data_, duration);
     else
-        start_timer(ctx_, data_, duration);
+        start_timer(ctx_, *data_, duration);
     data_->active = true;
 }
 
