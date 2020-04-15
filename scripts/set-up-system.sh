4253e81bd0fdebb023b3c82dddf88437ed8b4842
Thomas Madden <tmadden@gmail.com>
2020-04-15 14:31:10 -0400

    Expand testing to include C++17.

--- scripts/set-up-system.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set up a fresh Ubuntu Xenial installation so that it can build alia.
-# This should be run with sudo.
-echo "Setting up system..."
-set -x -e
-apt-get update -qy
-apt-get install -y --upgrade g++-5 gcc-5 lcov cmake git curl clang-4.0 llvm-4.0
