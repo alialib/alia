82dc6775ab123abfc4b3ae1d5d90bcbb4cc6ec0f
Thomas Madden <tmadden@gmail.com>
2020-07-16 13:36:51 -0400

    Refine signal directionality typing.

--- compilation_tests/read_write_intersection.cpp
+++ compilation_tests/read_write_intersection.cpp
@@ -5,8 +5,7 @@ using namespace alia;
 void
 f()
 {
-    signal_direction_intersection<read_only_signal, duplex_signal>::
-        type();
+    signal_direction_intersection<read_only_signal, duplex_signal>::type();
 #ifdef ALIA_TEST_COMPILATION_FAILURE
     signal_direction_intersection<read_only_signal, write_only_signal>::type();
 #endif
