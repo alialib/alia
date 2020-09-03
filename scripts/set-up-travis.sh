132409bf4b843c060a0d40e57aaf055b1b630eb5
Thomas Madden <tmadden@gmail.com>
2020-09-03 14:14:42 -0400

    Fix Travis build.

--- scripts/set-up-travis.sh
+++ scripts/set-up-travis.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Set up Travis for a "normal" CI build.
 set -x -e
+
+curl -sSL https://cmake.org/files/v3.17/cmake-3.17.2-Linux-x86_64.sh -o install-cmake.sh
+chmod +x install-cmake.sh
+./install-cmake.sh --prefix=/usr/local --skip-license
+
 pip install -r requirements.txt
+
 scripts/set-up-conan.sh
