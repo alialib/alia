a77a403eac23a8d53153f8bb25fa0f9113d171f1
Thomas Madden <tmadden@gmail.com>
2020-09-03 14:29:19 -0400

    Fix Travis CMake.

---
+++ scripts/install-cmake.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install a recent CMake on older Ubuntu systems.
+
+set -x -e
+
+curl -sSL https://cmake.org/files/v3.17/cmake-3.17.2-Linux-x86_64.sh -o install-cmake.sh
+chmod +x install-cmake.sh
+./install-cmake.sh --prefix=/usr/local --skip-license
