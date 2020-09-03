4641f1dd16cea127778411acf174c95634f056ed
Thomas Madden <tmadden@gmail.com>
2020-09-03 15:08:14 -0400

    Try CMake apt repo.

--- scripts/install-cmake.sh
+++ scripts/install-cmake.sh
@@ -3,8 +3,6 @@
 
 set -x -e
 
-sudo apt-get remove cmake
-
-curl -sSL https://cmake.org/files/v3.17/cmake-3.17.2-Linux-x86_64.sh -o install-cmake.sh
-chmod +x install-cmake.sh
-./install-cmake.sh --prefix=/usr/local --skip-license
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic-rc main'
+sudo apt-get update
+sudo apt-get install --upgrade cmake
