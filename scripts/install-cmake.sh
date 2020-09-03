b0456b6eb2d89dfc9d86513e597ac889a6ebd5c8
Thomas Madden <tmadden@gmail.com>
2020-09-03 14:51:08 -0400

    More work on Travis CMake.

--- scripts/install-cmake.sh
+++ scripts/install-cmake.sh
@@ -3,6 +3,8 @@
 
 set -x -e
 
+sudo apt-get remove cmake
+
 curl -sSL https://cmake.org/files/v3.17/cmake-3.17.2-Linux-x86_64.sh -o install-cmake.sh
 chmod +x install-cmake.sh
 ./install-cmake.sh --prefix=/usr/local --skip-license
