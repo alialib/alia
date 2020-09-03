12f65344b2867f7005c407345236e35efbdb82af
Thomas Madden <tmadden@gmail.com>
2020-09-03 15:15:59 -0400

    Another try at CMake apt repo.

--- scripts/install-cmake.sh
+++ scripts/install-cmake.sh
@@ -3,6 +3,8 @@
 
 set -x -e
 
+sudo apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic-rc main'
 sudo apt-get update
-sudo apt-get install --upgrade cmake
+sudo apt-get install cmake
