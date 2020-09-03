a38f2f524ba5836ed67142056c7a4fa559e08f43
Thomas Madden <tmadden@gmail.com>
2020-09-03 17:00:48 -0400

    Ditch fips. (#36)
    * Ditch fips.
    
    * Upgrade Travis to Focal.
    
    * Fix AppVeyor build.
    
    * Fix Travis build.
    
    * Fix Travis CMake.
    
    * More work on Travis CMake.
    
    * Try CMake apt repo.
    
    * Another try at CMake apt repo.
    
    * Go back to earlier CMake attempt.
    
    * Fix Travis build commands.
    
    * Fix LLVM commands.
    
    * Fix a warning.

--- scripts/do-ci-testing.sh
+++ scripts/do-ci-testing.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 # Do "normal" CI testing.
+
 set -x -e
+
 export CTEST_OUTPUT_ON_FAILURE=1
+
 python scripts/generate-distributables.py
 cp alia.hpp single_header_tests
+
 python scripts/generate-single-header-tests.py
-./fips make test linux-make-debug
-./fips make test linux-make-release
+
+mkdir debug-build
+cd debug-build
+cmake -DCMAKE_BUILD_TYPE=Debug -G"Unix Makefiles" ..
+cmake --build . --target ctest
+cd ..
+
+mkdir release-build
+cd release-build
+cmake -DCMAKE_BUILD_TYPE=Release -G"Unix Makefiles" ..
+cmake --build . --target ctest
+cd ..
--- scripts/set-up-travis.sh
+++ scripts/set-up-travis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Set up Travis for a "normal" CI build.
 set -x -e
+
 pip install -r requirements.txt
+
 scripts/set-up-conan.sh
