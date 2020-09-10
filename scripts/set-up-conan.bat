6e7343a312e274bb95eb01d933054d8461b565be
Thomas Madden <tmadden@gmail.com>
2020-09-10 14:14:54 -0400

    Remove Conan from main build.

--- scripts/set-up-conan.bat
+++
@@ -1,5 +0,0 @@
-:: Install additional Conan remotes that are needed to build alia.
-@echo off
-echo "Setting up Conan..."
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan || exit /b
-conan remote add catchorg https://api.bintray.com/conan/catchorg/Catch2
