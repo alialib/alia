17e2c82cf14bcf8a98fd9a3a577fcd846844fead
Thomas Madden <tmadden@gmail.com>
2020-09-14 19:40:39 -0400

    Fix the Travis build.

--- unit_tests/flow/testing.cpp
+++ unit_tests/flow/testing.cpp
@@ -14,3 +14,13 @@ check_log(std::string const& expected_contents)
     REQUIRE(the_log.str() == expected_contents);
     clear_log();
 }
+
+void
+do_object(test_context ctx, std::string name)
+{
+    tree_node<test_object>* node;
+    if (get_cached_data(ctx, &node))
+        node->object.name = name;
+    if (is_refresh_event(ctx))
+        refresh_tree_node(get<tree_traversal_tag>(ctx), *node);
+}
