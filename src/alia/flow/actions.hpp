2000a9f96b46a7c10fe0041005b6377e9ebc4634
Thomas Madden <tmadden@gmail.com>
2020-09-23 23:27:05 -0400

    Add write actions.

--- src/alia/flow/actions.hpp
+++ src/alia/flow/actions.hpp
@@ -467,6 +467,41 @@ actionize(Callable x)
     return lambda_action(std::move(x));
 }
 
+// add_write_action(signal, on_write) wraps signal in a similar signal that
+// will invoke on_write whenever the signal is written to. (on_write will be
+// passed the value that was written to the signal.)
+template<class Wrapped, class OnWrite>
+struct write_action_signal
+    : signal_wrapper<write_action_signal<Wrapped, OnWrite>, Wrapped>
+{
+    write_action_signal(Wrapped wrapped, OnWrite on_write)
+        : write_action_signal::signal_wrapper(std::move(wrapped)),
+          on_write_(std::move(on_write))
+    {
+    }
+    bool
+    ready_to_write() const
+    {
+        return this->wrapped_.ready_to_write() && on_write_.is_ready();
+    }
+    void
+    write(typename Wrapped::value_type value) const
+    {
+        perform_action(on_write_, value);
+        this->wrapped_.write(std::move(value));
+    }
+
+ private:
+    OnWrite on_write_;
+};
+template<class Wrapped, class OnWrite>
+auto
+add_write_action(Wrapped wrapped, OnWrite on_write)
+{
+    return write_action_signal<Wrapped, OnWrite>(
+        std::move(wrapped), std::move(on_write));
+}
+
 } // namespace alia
 
 #endif
