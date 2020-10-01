a308e3971301b803297c0e6bf640ac90940dd4df
Thomas Madden <tmadden@gmail.com>
2020-09-30 20:34:13 -0400

    Improve content ID mechanics.

--- unit_tests/flow/try_catch.cpp
+++ unit_tests/flow/try_catch.cpp
@@ -79,7 +79,8 @@ TEST_CASE("try_catch", "[flow][try_catch]")
     alia::system sys;
     initialize_system(sys, [&](context vanilla_ctx) {
         tree_traversal<test_object> traversal;
-        auto ctx = extend_context<tree_traversal_tag>(vanilla_ctx, traversal);
+        auto ctx = detail::add_context_object<tree_traversal_tag>(
+            vanilla_ctx, traversal);
         if (is_refresh_event(ctx))
         {
             traverse_object_tree(traversal, root, [&]() { controller(ctx); });
