0bf6a8a0e2f257948e2c984cfebe7ca0b9bc0c40
Thomas Madden <tmadden@gmail.com>
2020-12-31 15:35:20 -0500

    Fix the build

--- src/alia/html/document.hpp
+++ src/alia/html/document.hpp
@@ -1,9 +1,9 @@
 #ifndef ALIA_HTML_DOCUMENT_HPP
 #define ALIA_HTML_DOCUMENT_HPP
 
-#include <alia/html/context.hpp>
-
 #include <emscripten/val.h>
+
+#include <alia/html/context.hpp>
 
 namespace alia {
 namespace html {
