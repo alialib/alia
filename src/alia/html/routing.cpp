3414d89a8161e7a1eae12f8a6039e24676eb1889
Thomas Madden <tmadden@gmail.com>
2021-01-27 10:58:25 -0500

    Allow string literals on internal links

--- src/alia/html/routing.cpp
+++ src/alia/html/routing.cpp
@@ -8,6 +8,8 @@ get_location_hash(html::context ctx)
     return direct(const_cast<std::string const&>(get<system_tag>(ctx).hash));
 }
 
+namespace detail {
+
 element_handle
 internal_link(context ctx, readable<std::string> path)
 {
@@ -28,7 +30,9 @@ element_handle
 internal_link(
     context ctx, readable<std::string> text, readable<std::string> path)
 {
-    return internal_link(ctx, path).text(text);
+    return detail::internal_link(ctx, path).text(text);
 }
+
+} // namespace detail
 
 }} // namespace alia::html
