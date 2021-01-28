c4f219e972bcd849639cf60aa0177e20ebd3d5fb
Thomas Madden <tmadden@gmail.com>
2021-01-27 21:15:41 -0500

    Add is_empty

--- src/alia/signals/containers.hpp
+++ src/alia/signals/containers.hpp
@@ -3,19 +3,28 @@
 
 #include <alia/signals/adaptors.hpp>
 #include <alia/signals/application.hpp>
+#include <alia/signals/operators.hpp>
 
 namespace alia {
 
-// hide_if_empty(s), where :s is a signal, yields a wrapper for :s that will
-// claim to have no value if that value would be empty. (This is determined by
-// calling the empty() member function of the value, so it works on most
-// containers (including strings).)
-template<class Signal>
+// is_empty(s), where :s is a signal, yields a signal indicating whether or not
+// :s carries an empty value. (This is determined by calling the `empty()`
+// member function of the value, so it works on most containers (including
+// strings).)
+template<class ContainerSignal>
 auto
-hide_if_empty(Signal s)
+is_empty(ContainerSignal cs)
 {
-    return mask_reads(
-        s, lazy_apply([](auto const& x) { return !x.empty(); }, s));
+    return lazy_apply([](auto const& x) { return x.empty(); }, cs);
+}
+
+// hide_if_empty(s), where :s is a signal, yields a wrapper for :s that will
+// claim to have no value if that value would be empty.
+template<class ContainerSignal>
+auto
+hide_if_empty(ContainerSignal cs)
+{
+    return mask_reads(cs, !is_empty(cs));
 }
 
 } // namespace alia
