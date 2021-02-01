b19d1c15103ecac212ddbe467d174a39fdc936e3
Thomas Madden <tmadden@gmail.com>
2021-02-01 11:12:25 -0500

    Add web server script

---
+++ scripts/run-web-server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import BaseHTTPServer, SimpleHTTPServer
+
+port = 8002
+print "Running on port %d" % port
+
+SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map[
+    '.wasm'] = 'application/wasm'
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', port),
+                                  SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+print "Mapping \".wasm\" to \"%s\"" % \
+    SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm']
+httpd.serve_forever()
