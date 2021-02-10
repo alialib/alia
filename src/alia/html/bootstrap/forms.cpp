c49f2a6f609679c5a84f99c1d443163ffb0ea4b0
Thomas Madden <tmadden@gmail.com>
2021-02-10 10:27:31 -0500

    Fix checkbox

--- src/alia/html/bootstrap/forms.cpp
+++ src/alia/html/bootstrap/forms.cpp
@@ -9,24 +9,22 @@ checkbox(html::context ctx, duplex<bool> value, readable<std::string> label)
     bool checked = determinate && value.read();
     bool disabled = !value.ready_to_write();
 
-    return element(ctx, "div")
-        .class_("custom-control", "custom-checkbox")
-        .children([&] {
-            element(ctx, "input")
-                .attr("type", "checkbox")
-                .class_("custom-control-input")
-                .attr("disabled", disabled)
-                .attr("id", "custom-check-1")
-                .prop("indeterminate", !determinate)
-                .prop("checked", checked)
-                .callback("change", [&](emscripten::val e) {
-                    write_signal(value, e["target"]["checked"].as<bool>());
-                });
-            element(ctx, "label")
-                .class_("custom-control-label")
-                .attr("for", "custom-check-1")
-                .text(label);
-        });
+    return div(ctx, "custom-control custom-checkbox").children([&] {
+        element(ctx, "input")
+            .attr("type", "checkbox")
+            .attr("class", "custom-control-input")
+            .attr("disabled", disabled)
+            .attr("id", "custom-check-1")
+            .prop("indeterminate", !determinate)
+            .prop("checked", checked)
+            .callback("change", [&](emscripten::val e) {
+                write_signal(value, e["target"]["checked"].as<bool>());
+            });
+        element(ctx, "label")
+            .class_("custom-control-label")
+            .attr("for", "custom-check-1")
+            .text(label);
+    });
 }
 
 }}}} // namespace alia::html::bootstrap::detail
