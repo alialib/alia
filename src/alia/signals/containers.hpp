d55b618a43210ac31a2361b2a792669f4e237b1f
Thomas Madden <tmadden@gmail.com>
2021-02-10 22:34:49 -0500

    Add container signal size query

--- src/alia/signals/containers.hpp
+++ src/alia/signals/containers.hpp
@@ -7,6 +7,15 @@
 
 namespace alia {
 
+// size(s), where :s is a signal, yields a signal carrying the size of :s
+// (as determined by calling s.size()).
+template<class ContainerSignal>
+auto
+size(ContainerSignal cs)
+{
+    return lazy_apply(ALIA_MEM_FN(size), std::move(cs));
+}
+
 // is_empty(s), where :s is a signal, yields a signal indicating whether or not
 // :s carries an empty value. (This is determined by calling the `empty()`
 // member function of the value, so it works on most containers (including
