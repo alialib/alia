8560d8cf54252c2484a67e6db54cc2849fb63031
Thomas Madden <tmadden@gmail.com>
2021-03-10 17:24:59 -0500

    Improve CMake fetching

--- cmake/json.cmake
+++ cmake/json.cmake
@@ -1,10 +1,11 @@
 include(FetchContent)
+
+message(STATUS "Fetching nlohmann/json")
+
 FetchContent_Declare(json
-  GIT_REPOSITORY https://github.com/nlohmann/json
-  GIT_TAG v3.7.3
-)
-FetchContent_GetProperties(json)
-if(NOT json_POPULATED)
-  FetchContent_Populate(json)
-  include_directories(${json_SOURCE_DIR}/single_include/)
-endif()
+  GIT_REPOSITORY
+  https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent
+  GIT_TAG v3.9.1
+  GIT_SHALLOW TRUE)
+
+FetchContent_MakeAvailable(json)
