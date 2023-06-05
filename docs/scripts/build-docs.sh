0244540cf82e74f55ed2ab5c7aacd778ec8ba11d
Thomas Madden <tmadden@gmail.com>
2023-06-05 07:37:40 -0400

    Fix docs build

--- docs/scripts/build-docs.sh
+++ docs/scripts/build-docs.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # Build the documentation in the "www" directory.
-# Note that the demos must be built in build before running this script.
+# Note that the demos must be built in `<project-root>/build/emscripten` before
+# running this script.
 set -e
 mkdir -p www
 cd www
 rm -rf *
 cp ../docs/* .
 unzip favicon.zip && rm *.zip
-cp ../../build/*.js ../../build/*.wasm .
+cp ../../build/emscripten/asm-dom.js .
+cp ../../build/emscripten/docs/demos.js .
+cp ../../build/emscripten/docs/demos.wasm .
 wget https://github.com/alialib/alia/releases/latest/download/alia.hpp
 ../scripts/build-dot-files.sh
 touch .nojekyll
