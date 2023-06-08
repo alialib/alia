de899e997e0abc221d7bec8c3a3af8d8661d8abb
Thomas Madden <tmadden@gmail.com>
2023-06-08 08:49:11 -0400

    Fix set-up-python.sh

--- scripts/set-up-python.sh
+++ scripts/set-up-python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set up a Python virtual environment so that it can build alia.
-echo "Setting up Python environment in .python..."
+echo "Setting up Python environment in .venv..."
 set -x -e
 virtualenv --prompt="(alia) " "$@" .venv
 source .venv/bin/activate
