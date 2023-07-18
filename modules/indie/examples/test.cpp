5e952b6c8214b411a165cf85436385a802ec36c4
Thomas Madden <tmadden@gmail.com>
2023-07-18 18:52:27 -0400

    Do some more prototyping

--- modules/indie/examples/test.cpp
+++ modules/indie/examples/test.cpp
@@ -1,21 +1,19 @@
-#include <alia/indie/backends/glfw.hpp>
-#include <alia/indie/layout/api.hpp>
-#include <alia/indie/layout/utilities.hpp>
-#include <alia/indie/rendering.hpp>
-#include <alia/indie/system/object.hpp>
+#include <alia/indie.hpp>
 
-#include <GLFW/glfw3.h>
+#include <color/color.hpp>
+
+#include <cmath>
 
 using namespace alia;
 
-struct my_render_node : indie::render_node
+struct box_node : indie::render_node
 {
     virtual void
     render(SkCanvas& canvas)
     {
         SkPaint paint;
-        paint.setColor(SK_ColorMAGENTA);
-        auto const& region = this->layout.assignment().region;
+        paint.setColor(color_);
+        auto const& region = this->layout_.assignment().region;
         SkRect rect;
         rect.fLeft = SkScalar(region.corner[0]);
         rect.fTop = SkScalar(region.corner[1]);
@@ -24,29 +22,78 @@ struct my_render_node : indie::render_node
         canvas.drawRect(rect, paint);
     }
 
-    alia::layout_leaf layout;
+    SkColor color_;
+    alia::layout_leaf layout_;
 };
 
-int
-main(void)
+void
+do_box(indie::context ctx, SkColor color)
 {
-    indie::glfw_window the_window(
-        "alia test",
-        make_vector<unsigned>(1200, 1600),
-        [](indie::context ctx) {
-            if (is_refresh_event(ctx))
-            {
-                static my_render_node my_node;
-                add_render_node(
-                    get<indie::render_traversal_tag>(ctx), &my_node);
-                my_node.layout.refresh_layout(
-                    get<indie::layout_traversal_tag>(ctx),
-                    alia::layout(TOP | LEFT),
-                    alia::leaf_layout_requirements(
-                        alia::make_layout_vector(100, 100), 0, 0));
-                add_layout_node(
-                    get<indie::layout_traversal_tag>(ctx), &my_node.layout);
-            }
-        });
-    the_window.do_main_loop();
+    if (is_refresh_event(ctx))
+    {
+        auto& node = get_cached_data<box_node>(ctx);
+        node.color_ = color;
+        add_render_node(get<indie::render_traversal_tag>(ctx), &node);
+        node.layout_.refresh_layout(
+            get<indie::layout_traversal_tag>(ctx),
+            alia::layout(TOP | LEFT | PADDED),
+            alia::leaf_layout_requirements(
+                alia::make_layout_vector(100, 100), 0, 0));
+        add_layout_node(get<indie::layout_traversal_tag>(ctx), &node.layout_);
+    }
+}
+
+namespace alia { namespace indie {
+
+layout_traversal&
+get_layout_traversal(context ctx)
+{
+    return get<indie::layout_traversal_tag>(ctx);
+}
+
+}} // namespace alia::indie
+
+struct simple_render_container : indie::render_container
+{
+    void
+    render(SkCanvas& canvas)
+    {
+        indie::render_children(canvas, *this);
+    }
+};
+
+void
+my_ui(indie::context ctx)
+{
+    static simple_render_container container;
+    indie::scoped_render_container container_scope;
+    if (is_refresh_event(ctx))
+    {
+        container_scope.begin(get<indie::render_traversal_tag>(ctx), &container);
+    }
+    row_layout row(ctx);
+    do_box(ctx, SK_ColorMAGENTA);
+
+    color::yiq<std::uint8_t> y1 = ::color::constant::blue_t{};
+    color::yiq<std::uint8_t> y2 = ::color::constant::red_t{};
+    color::yiq<std::uint8_t> yr = color::operation::mix(
+        y1,
+        std::min(
+            1.0,
+            std::fabs(std::sin(get_raw_animation_tick_count(ctx) / 1000.0))),
+        y2);
+    color::rgb<std::uint8_t> r(yr);
+
+    do_box(
+        ctx,
+        SkColorSetARGB(
+            0xff,
+            ::color::get::red(r),
+            ::color::get::green(r),
+            ::color::get::blue(r)));
+
+    static SkColor clicky_color = SK_ColorRED;
+    event_handler<indie::mouse_click_event>(
+        ctx, [&](auto, auto&) { clicky_color = SK_ColorBLUE; });
+    do_box(ctx, clicky_color);
 }
