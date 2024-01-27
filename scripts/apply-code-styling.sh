b09360020e45f35e7a39c8c625066bbf194d64b1
Thomas Madden <tmadden@gmail.com>
2024-01-27 16:33:01 -0500

    WIP

--- scripts/apply-code-styling.sh
+++ scripts/apply-code-styling.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Apply our .clang-format styling to all C++ source files.
 # Note that this should be run from the root directory.
-find core docs modules -name *.[chi]pp \
+find docs integrations src -name *.[chi]pp \
     | xargs -n1 clang-format -i -style=file
