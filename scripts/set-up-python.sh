8e88b94f43b44bab974faae9ef7e6b8079c13b6d
Thomas Madden <tmadden@gmail.com>
2024-01-27 16:33:01 -0500

    WIP

--- scripts/set-up-python.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Set up a Python virtual environment so that it can build alia.
-echo "Setting up Python environment in .venv..."
-set -x -e
-virtualenv --prompt="(alia) " "$@" .venv
-source .venv/bin/activate
-python --version
-pip install gcovr
