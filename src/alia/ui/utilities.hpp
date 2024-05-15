fad8362c59b283961677699b3531c6057d9f7011
Thomas Madden <tmadden@gmail.com>
2024-05-15 09:36:56 -0400

    Add ui/utilities.hpp

---
+++ src/alia/ui/utilities.hpp
@@ -0,0 +1,11 @@
+#ifndef ALIA_UI_UTILITIES_HPP
+#define ALIA_UI_UTILITIES_HPP
+
+#include <alia/ui/layout/utilities.hpp>
+#include <alia/ui/utilities/keyboard.hpp>
+#include <alia/ui/utilities/mouse.hpp>
+#include <alia/ui/utilities/regions.hpp>
+#include <alia/ui/utilities/rendering.hpp>
+#include <alia/ui/utilities/skia.hpp>
+
+#endif
