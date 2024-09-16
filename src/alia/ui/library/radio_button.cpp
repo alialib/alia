82fc837820ee255369d8bb4557e1aa0b709e8019
Thomas Madden <tmadden@mgh.harvard.edu>
2024-09-16 15:26:32 -0400

    Do some minor refactoring

--- src/alia/ui/library/radio_button.cpp
+++ src/alia/ui/library/radio_button.cpp
@@ -33,6 +33,94 @@ struct radio_button_data
 };
 
 void
+render_radio_button(
+    dataless_ui_context ctx,
+    radio_button_data& data,
+    bool selected,
+    widget_state state)
+{
+    auto& event = cast_event<render_event>(ctx);
+    SkCanvas& canvas = *event.canvas;
+
+    auto const& region = data.layout_node.assignment().region;
+
+    SkRect rect;
+    rect.fLeft = SkScalar(region.corner[0]);
+    rect.fTop = SkScalar(region.corner[1]);
+    rect.fRight = SkScalar(region.corner[0] + region.size[0]);
+    rect.fBottom = SkScalar(region.corner[1] + region.size[1]);
+
+    if (event.canvas->quickReject(rect))
+        return;
+
+    auto center = get_center(region);
+
+    if ((state & WIDGET_PRIMARY_STATE_MASK) == WIDGET_DISABLED)
+    {
+        {
+            SkPaint paint;
+            paint.setAntiAlias(true);
+            paint.setStyle(SkPaint::kStroke_Style);
+            paint.setColor(SkColorSetARGB(0xff, 0x60, 0x60, 0x66));
+            paint.setStrokeWidth(3);
+            canvas.drawPath(SkPath::Circle(center[0], center[1], 15.f), paint);
+        }
+
+        if (selected)
+        {
+            SkPaint paint;
+            paint.setAntiAlias(true);
+            paint.setColor(SkColorSetARGB(0xff, 0x80, 0x80, 0x88));
+            paint.setStrokeWidth(3);
+            canvas.drawPath(SkPath::Circle(center[0], center[1], 10), paint);
+        }
+
+        return;
+    }
+
+    if ((state & WIDGET_PRIMARY_STATE_MASK) == WIDGET_HOT
+        || (state & WIDGET_PRIMARY_STATE_MASK) == WIDGET_DEPRESSED)
+    {
+        SkPaint paint;
+        paint.setAntiAlias(true);
+        paint.setColor(SkColorSetARGB(0x20, 0xff, 0xff, 0xff));
+        canvas.drawPath(SkPath::Circle(center[0], center[1], 32.f), paint);
+    }
+
+    float smoothed_state = smooth_between_values(
+        ctx,
+        data.bits,
+        2,
+        selected,
+        1.f,
+        0.f,
+        animated_transition{default_curve, 200});
+
+    rgb8 color = interpolate(
+        rgb8(0xa0, 0xa0, 0xa0), rgb8(0x90, 0xc0, 0xff), smoothed_state);
+
+    render_click_flares(
+        ctx, data.bits, data.click_flare_bits, state, center, color);
+
+    {
+        SkPaint paint;
+        paint.setAntiAlias(true);
+        paint.setStyle(SkPaint::kStroke_Style);
+        paint.setColor(SkColorSetARGB(0xff, 0xc0, 0xc0, 0xc0));
+        paint.setStrokeWidth(3);
+        canvas.drawPath(SkPath::Circle(center[0], center[1], 15.f), paint);
+    }
+
+    float dot_radius = interpolate(0.f, 10.f, smoothed_state);
+
+    SkPaint paint;
+    paint.setAntiAlias(true);
+    paint.setColor(SkColorSetARGB(0xff, 0x90, 0xc0, 0xff));
+    paint.setStrokeWidth(3);
+    canvas.drawPath(SkPath::Circle(center[0], center[1], dot_radius), paint);
+}
+
+void
 do_radio_button(
     ui_context ctx,
     duplex<bool> selected,
@@ -86,82 +174,6 @@ do_radio_button(
             break;
 
         case RENDER_CATEGORY: {
-            auto& event = cast_event<render_event>(ctx);
-
-            SkCanvas& canvas = *event.canvas;
-
-            auto const& region = data.layout_node.assignment().region;
-
-            SkRect rect;
-            rect.fLeft = SkScalar(region.corner[0]);
-            rect.fTop = SkScalar(region.corner[1]);
-            rect.fRight = SkScalar(region.corner[0] + region.size[0]);
-            rect.fBottom = SkScalar(region.corner[1] + region.size[1]);
-
-            if (event.canvas->quickReject(rect))
-                break;
-
-            auto center = get_center(region);
-
-            if (is_disabled)
-            {
-                {
-                    SkPaint paint;
-                    paint.setAntiAlias(true);
-                    paint.setStyle(SkPaint::kStroke_Style);
-                    paint.setColor(SkColorSetARGB(0xff, 0x60, 0x60, 0x66));
-                    paint.setStrokeWidth(3);
-                    canvas.drawPath(
-                        SkPath::Circle(center[0], center[1], 15.f), paint);
-                }
-
-                if (condition_is_true(selected))
-                {
-                    SkPaint paint;
-                    paint.setAntiAlias(true);
-                    paint.setColor(SkColorSetARGB(0xff, 0x80, 0x80, 0x88));
-                    paint.setStrokeWidth(3);
-                    canvas.drawPath(
-                        SkPath::Circle(center[0], center[1], 10), paint);
-                }
-
-                break;
-            }
-
-            uint8_t highlight = 0;
-            if (is_click_in_progress(ctx, id, mouse_button::LEFT)
-                || is_pressed(data.keyboard_click_state_))
-            {
-                // highlight = 0x40;
-                highlight = 0x20;
-            }
-            else if (is_click_possible(ctx, id))
-            {
-                highlight = 0x20;
-            }
-            if (highlight != 0)
-            {
-                SkPaint paint;
-                paint.setAntiAlias(true);
-                paint.setColor(SkColorSetARGB(highlight, 0xff, 0xff, 0xff));
-                canvas.drawPath(
-                    SkPath::Circle(center[0], center[1], 32.f), paint);
-            }
-
-            float smoothed_state = smooth_between_values(
-                ctx,
-                data.bits,
-                2,
-                condition_is_true(selected),
-                1.f,
-                0.f,
-                animated_transition{default_curve, 200});
-
-            rgb8 color = interpolate(
-                rgb8(0xa0, 0xa0, 0xa0),
-                rgb8(0x90, 0xc0, 0xff),
-                smoothed_state);
-
             auto state = get_widget_state(
                 ctx,
                 id,
@@ -169,28 +181,8 @@ do_radio_button(
                     | (is_pressed(data.keyboard_click_state_)
                            ? WIDGET_DEPRESSED
                            : NO_FLAGS));
-
-            render_click_flares(
-                ctx, data.bits, data.click_flare_bits, state, center, color);
-
-            {
-                SkPaint paint;
-                paint.setAntiAlias(true);
-                paint.setStyle(SkPaint::kStroke_Style);
-                paint.setColor(SkColorSetARGB(0xff, 0xc0, 0xc0, 0xc0));
-                paint.setStrokeWidth(3);
-                canvas.drawPath(
-                    SkPath::Circle(center[0], center[1], 15.f), paint);
-            }
-
-            float dot_radius = interpolate(0.f, 10.f, smoothed_state);
-
-            SkPaint paint;
-            paint.setAntiAlias(true);
-            paint.setColor(SkColorSetARGB(0xff, 0x90, 0xc0, 0xff));
-            paint.setStrokeWidth(3);
-            canvas.drawPath(
-                SkPath::Circle(center[0], center[1], dot_radius), paint);
+            render_radio_button(ctx, data, condition_is_true(selected), state);
+            break;
         }
     }
     alia_end
