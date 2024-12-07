f20cd74595ac642f269293d525ed1a065ee75baf
Thomas Madden <tmadden@gmail.com>
2024-12-07 13:17:29 -0500

    Get a demo working

--- src/alia/ui/library/separator.hpp
+++ src/alia/ui/library/separator.hpp
@@ -6,7 +6,7 @@
 namespace alia {
 
 void
-do_separator(ui_context ctx, layout const& layout_spec);
+do_separator(ui_context ctx, layout const& layout_spec = default_layout);
 
 } // namespace alia
 
