62cb25d59a2ae1bc5c2f1d5ff5aa6dacfd49e61a
Thomas Madden <tmadden@gmail.com>
2025-05-09 22:50:42 -0400

    Prototype context

--- core/ui/include/alia/ui/context.hpp
+++ core/ui/include/alia/ui/context.hpp
@@ -1,22 +1,40 @@
 #pragma once
 
-#include <alia/ui/display_list.hpp>
+#include <cstddef>
 
 namespace alia {
 
 struct Event;
 struct DisplayList;
 struct System;
+struct LayoutSpec;
+struct LayoutPlacement;
 
 enum class PassType
 {
+    Refresh,
     Draw,
     Event,
+};
+
+struct LayoutEmission
+{
+    LayoutSpec* specs;
+    std::size_t count;
+};
+
+struct LayoutConsumption
+{
+    LayoutPlacement* placements;
+    std::size_t index;
 };
 
 struct Pass
 {
     PassType type;
+    // TODO: Sort this out.
+    LayoutEmission layout_emission;
+    LayoutConsumption layout_consumption;
     DisplayList* display_list;
     Event* event;
 };
