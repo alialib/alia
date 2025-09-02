225a3f98789e652c67c40abf3615ee3c917079f9
Thomas Madden <tmadden@mgh.harvard.edu>
2025-09-02 14:43:34 -0400

    Add substrate destructors test

--- tests/kernel/test_substrate.cpp
+++ tests/kernel/test_substrate.cpp
@@ -8,6 +8,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <new>
+#include <vector>
 
 namespace {
 
@@ -136,6 +137,7 @@ TEST_CASE("basic_block")
 
     substrate_end_traversal(traversal);
 
+    destruct_substrate_block(system, block);
     destruct_substrate_system(system);
 }
 
@@ -207,6 +209,7 @@ TEST_CASE("basic_block_discovery")
 
     substrate_end_traversal(traversal);
 
+    destruct_substrate_block(system, block);
     destruct_substrate_system(system);
 }
 
@@ -277,7 +280,89 @@ TEST_CASE("substrate_use_block")
 
     substrate_end_traversal(traversal);
 
+    destruct_substrate_block(system, block);
     destruct_substrate_system(system);
+}
+
+TEST_CASE("substrate_destructors")
+{
+    scratch_rig rig;
+
+    alia::substrate_system system;
+
+    alia::substrate_allocator allocator;
+    allocator.user_data = nullptr;
+    allocator.alloc = block_alloc;
+    allocator.free = block_free;
+
+    construct_substrate_system(system, allocator);
+
+    alia::substrate_traversal traversal = {};
+    substrate_begin_traversal(traversal, system, rig.scratch);
+
+    alia::substrate_block_spec spec;
+    spec.size = 1024;
+    spec.alignment = 16;
+
+    alia::substrate_block_scope scope;
+    alia::substrate_block block;
+
+    substrate_begin_block(traversal, scope, block, spec);
+
+    std::vector<int> destructions;
+
+    struct test_obejct
+    {
+        std::vector<int>* destructions = nullptr;
+        int id = 0;
+    };
+
+    auto use_object = [&](int id) {
+        auto result = substrate_use_object(
+            traversal, sizeof(test_obejct), alignof(test_obejct), [](void* p) {
+                auto* obj = static_cast<test_obejct*>(p);
+                obj->destructions->push_back(obj->id);
+            });
+        CHECK(result.ptr != nullptr);
+        CHECK(result.mode == alia::substrate_block_traversal_mode::INIT);
+        auto* obj = new (result.ptr) test_obejct;
+        obj->destructions = &destructions;
+        obj->id = id;
+        return obj;
+    };
+
+    use_object(1);
+    use_object(2);
+
+    auto subblock = substrate_use_block(traversal);
+    CHECK(subblock != nullptr);
+    CHECK(subblock->system == &system);
+    CHECK(subblock->storage == nullptr);
+    CHECK(subblock->destructors == nullptr);
+
+    alia::substrate_block_scope subscope;
+
+    substrate_begin_block(traversal, subscope, *subblock, spec);
+
+    use_object(3);
+    use_object(4);
+
+    substrate_end_block(traversal, subscope);
+
+    use_object(5);
+    use_object(6);
+
+    substrate_end_block(traversal, scope);
+
+    substrate_end_traversal(traversal);
+
+    CHECK(destructions.size() == 0);
+
+    destruct_substrate_block(system, block);
+    destruct_substrate_system(system);
+
+    CHECK(destructions.size() == 6);
+    CHECK(destructions == std::vector<int>({6, 5, 4, 3, 2, 1}));
 }
 
 TEST_CASE("substrate_generations")
